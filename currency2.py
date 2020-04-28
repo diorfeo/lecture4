42fd1c879f79fe0749877326bc0a29169ab44922
diorfeo <john.sebastian.diaz@gmail.com>
2020-04-28 17:07:20 -0400

    samples API and ORM

--- currency2.py
+++ currency2.py
@@ -3,13 +3,14 @@ import requests
 def main():
     base = input("First Currency: ")
     other = input("Second Currency: ")
-    res = requests.get("https://api.fixer.io/latest",
-                       params={"base": base, "symbols": other})
+    res = requests.get("http://data.fixer.io/api/latest",
+                       params={"access_key":"e966f003d2d4f31a7288158320c9bd05",
+                               "symbols": base +", "+other})
     if res.status_code != 200:
         raise Exception("ERROR: API request unsuccessful.")
     data = res.json()
     rate = data["rates"][other]
-    print(f"1 {base} is equal to {rate} {other}")
+    print(f"1 EUR is equal to {rate} {other}")
 
 if __name__ == "__main__":
     main()
